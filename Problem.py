5fe15991328c2426d9360ceabeb350e94b027117
jagritikush <jagritikushwaha005@gmail.com>
2026-02-26 01:43:42 +0530

    Add files via upload

---
+++ Problem.py
@@ -0,0 +1,13 @@
+def rotate_clockwise(arr):
+    n= len(arr)
+    for i in range(n-1,0,-1):
+        arr[i]=arr[i-1]
+        
+    arr[0]=arr[-1]
+    return arr
+
+arr = [1,2,3,4,5]
+rotated_arr = rotate_clockwise(arr)
+print("Rotated array:",rotated_arr)
+
+
